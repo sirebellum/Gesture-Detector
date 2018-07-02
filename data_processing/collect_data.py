1fa15167dfceac2dd62a60b85d5f050aba17961c
Sirebellum <joshua.herrera227@gmail.com>
2018-07-02 03:15:10 -0500

    Various user input methods for accessing dummy modes

--- data_processing/collect_data.py
+++ data_processing/collect_data.py
@@ -1,11 +1,22 @@
 import cv2
 import kpdetection #Uses Detectron API to return keypoints
 import detectron.utils.vis as vis_utils
+import numpy as np
 
 #Pickle dictionary for use in svm
 def store_data(keypoints):
     
+    return False
     
+#Review dictionary for use in svm
+def review_data(keypoints):
+    
+    return False
+    
+#Normalize keypoints within a square
+def normalize_data(keypoints):
+    
+    kps_norm = {key: np.asarray(keypoints[key]) for key in keypoints}
     
     return False
 
@@ -22,20 +33,20 @@ keypoints = {clas: list() for clas in classes}
 print "Collecting data for ", keypoints.keys()
 
 #Begin data collection
-collect = "y"
+driver = "c"
 try:
-  while collect == "y":
+  while driver == "c":
   
     #Define class for this round
     print classes
-    clas = raw_input("what class would you like to collect for?: ")
-    record = ""
+    clas = raw_input("what class would you like to record for?: ")
+    record = ''
     if clas not in classes:
        print "Please specify a real class"
        record = "n"
     
     #Visualize and store keypoints
-    while record == "": #while enter is the only thing pressed
+    while record == '': #while enter is the only thing pressed
         #Read next frame
         _, frame = media.read()
         if frame is None: #end of stream
@@ -65,9 +76,17 @@ try:
     print "Done recording for", clas+"."
     for clas in classes:
         print len(keypoints[clas]), "total keypoints recorded for", clas+"."
-    collect = raw_input("Continue recording data? (y/n): ")
-    
+    driver = raw_input("Continue, review, or quit? (c/r/q): ")
+  
+  #fit all keypoints within box
+  normalize_data(keypoints)
+  
+  if driver == "r":
+      review_data(keypoints)
+  
   store_data(keypoints)
+  kpdetection.cleanup()
+  exit()
 
 #Stop data collection, ask to store
 except KeyboardInterrupt:
